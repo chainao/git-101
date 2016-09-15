b530507c33e7b757a0686e2b82d0d6f562037fb4
Chaina <chaina.oliveira@gmail.com>
2016-09-15 11:39:10 -0300

    Implementação da função fibonacci

--- src/com/example/ClasseTeste.java
+++ src/com/example/ClasseTeste.java
@@ -4,6 +4,18 @@ public class ClasseTeste {
 	
 	public static void main(String[] args) {
 		System.out.println("Main method");
+		
+		//Imprime os 10 primiro termos da sequência de fibonacci
+		for (int i = 0; i < 10; i++) {
+            System.out.print(fibonacci(i) + "\t");
+        }
 	}
-
+	
+	public static long fibonacci(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 }
